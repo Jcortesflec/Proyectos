37ff7153a26ccafcf39f83af6505d76d54c28023
LoanSnow <85372994+Jcortesflec@users.noreply.github.com>
2023-02-09 20:21:17 +0000

    Imprimir Hola Mundo

---
+++ main.py
@@ -0,0 +1,20 @@
+""""nombre='juan'
+apellido='cortes'
+edad = 24
+template = f'hola carecola, mi nombre es {nombre} y mi apellido es {apellido} y mi edad es {edad}'
+print (template)
+## esto es operaciones simples ##
+denominador = 0
+numerador = 0
+promedio = []
+promedio.append(int(input('coloca un valor para el mes de enero ')))
+promedio.append(int(input('coloca un valor para el mes de febrero ')))
+promedio.append(int(input('coloca un valor para el mes de marzo ')))
+for promedio in promedio:
+  denominador+=1
+  numerador = numerador + promedio
+resultado = numerador/denominador
+print(f"este es el valor promedio para los meses agregados {resultado}")"""
+print("Hola mundo!")
+
+
